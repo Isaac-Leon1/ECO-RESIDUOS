f55248d228d49ca7bca9a95126c61a698cc555cf
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-07-28 18:50:07 -0500

    .

--- frontend/src/pages/ListarRutas.jsx
+++ frontend/src/pages/ListarRutas.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import axios from "axios";
 
 const ListarRutas = () => {
-
   return (
-    <div>
-      <nav>
-      <img src="/public/logo.png" className="w-80 mx-auto pb-10"></img>
-        <ul>
-          <li>Inicio</li>
-          <li>Listar rutas</li>
-          <li>Reportes de ciudadanos</li>
-        </ul>
-        <h1 className="text-[#06457C]">Listar Rutas</h1>
-      </nav>
-    </div>
+    <>
+      <div>
+        <nav>
+          <img src="/public/logo.png" className="w-80 mx-auto pb-10"></img>
+          <ul>
+            <li>Inicio</li>
+            <li>Listar rutas</li>
+            <li>Reportes de ciudadanos</li>
+          </ul>
+          <h1 className="text-[#06457C]">Listar Rutas</h1>
+        </nav>
+      </div>
+    </>
   );
 };
 export default ListarRutas;
