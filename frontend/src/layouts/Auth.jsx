f765394e7f9dd0f9438a6c0b585204b8178a26a2
Isaac4Lion <92281039+Isaac4Lion@users.noreply.github.com>
2024-07-30 21:59:58 -0500

    Contexto Autenticacion

--- frontend/src/layouts/Auth.jsx
+++ frontend/src/layouts/Auth.jsx
@@ -1,6 +1,9 @@
 import {Navigate, Outlet} from "react-router-dom";
 
 const Auth = () => {
-    return (<main className="min-h-screen flex justify-center w-full" ><Outlet></Outlet></main>)
+    return (
+    <main className="min-h-screen flex justify-center w-full" >
+        <Outlet />
+    </main>)
 }
 export default Auth;
